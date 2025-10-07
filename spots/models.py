4c3757566ec1cac35ec5e50cd586982cc54b25ee
1f10230237 <s1f102302371@iniad.org>
2025-10-07 17:55:39 +0900

    投稿基盤追加

--- spots/models.py
+++ spots/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Spot(models.Model):
     title = models.CharField(max_length=100)
@@ -11,3 +12,13 @@ class Spot(models.Model):
 
     def __str__(self):
         return self.title
+
+class Post(models.Model):
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE, related_name='posts', null=True, blank=True)  # ← ここを修正
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author.username} - {self.spot.title if self.spot else 'No Spot'}"
