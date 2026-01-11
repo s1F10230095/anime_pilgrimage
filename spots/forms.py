2e8fbc73f6bfb6388292e74e2182d51345a46e64
1f10230237 <s1f102302371@iniad.org>
2026-01-11 22:43:18 +0900

    ハッシュタグの追加

--- spots/forms.py
+++ spots/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
         model = Post
         fields = ['text', 'image']  # モデルに存在するフィールド名に合わせる
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['spot', 'text', 'image']
